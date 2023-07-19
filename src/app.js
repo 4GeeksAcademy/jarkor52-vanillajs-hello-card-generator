3f687aacfa071f26d6a28e7b03576938c33896b6
jarkor5 <67030533+jarkor5@users.noreply.github.com>
2023-07-19 22:42:05 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const suits = ["heart", "spade", "club", "diamond"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jota", "Reina", "Rey", "As"];
+
+const cardElement = document.getElementById("card");
+
+function generateRandomCard() {
+  const suitIndex = Math.floor(Math.random() * suits.length);
+  const valueIndex = Math.floor(Math.random() * values.length);
+
+  const suit = suits[suitIndex];
+  const value = values[valueIndex];
+
+  cardElement.className = `card ${suit}`;
+
+  cardElement.textContent = `${value} ${getSuitSymbol(suit)}`;
+}
+
+function getSuitSymbol(suit) {
+  switch (suit) {
+    case "heart":
+      return "♥";
+    case "spade":
+      return "♠";
+    case "club":
+      return "♣";
+    case "diamond":
+      return "♦";
+    default:
+      return "";
+  }
+}
+
+generateRandomCard();
+
+cardElement.addEventListener("click", generateRandomCard);
